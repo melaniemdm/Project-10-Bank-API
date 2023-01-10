99017c4ce6750d228b1e63592c52ee3cfa8f400e
melaniemdm <melanieborja.pro@gmail.com>
2023-01-10 11:49:58 +0100

    feature(js) utils/modal

---
+++ client/src/Utils/modal.js
@@ -0,0 +1,11 @@
+//ouverture et fermeture de la modale edit name
+
+export function openModal(){
+document.querySelector('.containerUser').style.display = 'flex';
+
+}
+
+export function closeModal(){
+    document.querySelector('.containerUser').style.display = 'none';
+    
+}
