c6031bd3e737c6a7cc2169109526f268debe5330
melaniemdm <melanieborja.pro@gmail.com>
2023-01-10 12:11:24 +0100

    feature(js & css) open modal profil with backend information

--- client/src/components/ProfilUser/index.js
+++ client/src/components/ProfilUser/index.js
@@ -1,19 +1,32 @@
 
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
+import { closeModal } from '../../Utils/modal';
 import './style.scss';
 
 export default function ProfilUser(){
-     
-
-    return <div className="sign-in-content"><div className="input-wrapper">
+  const [firstName, setFirstName] = useState('toto')   
+  const [lastName, setLastName] = useState('titi')  
+  useEffect(()=> {
+    
+    async function getProfile(){
+      const response = await Axios.post("http://localhost:3001/api/v1/user/profile", {}, {headers: { Authorization: `Bearer `+ sessionStorage.getItem("token") }} )
+      setFirstName(response.data.body.firstName)
+      setLastName(response.data.body.lastName)
+    }  
+    getProfile()
+    
+    },[])
+    return <div className="sign-in-content containerUser" ><div className="input-wrapper">
         <h1> Settings User</h1>
-<div>LastName</div>
-<input id='lastName' placeholder="lastName"></input>
+<div>LastName : </div>
+<input id='lastName' type="text" value={firstName} onChange={(e)=>setFirstName(e.target.value)}></input>
 
-<div>FirstName</div>
-<input id='firstName' placeholder="FirstName"></input>
+<div>FirstName : </div>
+<input id='firstName'  type="text" value={lastName} onChange={(e)=>setLastName(e.target.value)}></input>
 
 <div id="mail">Mail : toto@gmail.com</div>
 
-<button className="sign-in-button">Valider</button>
+<button className="sign-in-button" onClick={()=>closeModal()}>Valider</button>
     </div></div>
 }
--- client/src/components/UserDetail/index.js
+++ client/src/components/UserDetail/index.js
@@ -2,10 +2,13 @@ import './style.scss';
 import CardAccount from '../CardAccount';
 import { useEffect, useState } from "react";
 import Axios from 'axios';
+import { openModal } from '../../Utils/modal';
+import ProfilUser from '../ProfilUser';
 
 export default function UserDetail() {
 
 const [userName, setUserName] = useState("");
+
 useEffect(()=> {
 async function getProfile(){
   const response = await Axios.post("http://localhost:3001/api/v1/user/profile", {}, {headers: { Authorization: `Bearer `+ sessionStorage.getItem("token") }} )
@@ -17,15 +20,16 @@ getProfile()
 
 return <main className="main bg-dark">
       <div className="header">
+        
         <h1>Welcome back<br />{userName}!</h1>
-        <button className="edit-button">Edit Name</button>
+        <button className="edit-button" onClick={()=> openModal()}>Edit Name</button>
       </div>
       <h2 className="sr-only">Accounts</h2>
 
       <CardAccount  titleAccount="Argent Bank Checking (x8349)" dollars="$2,082.79" accountDescription ="Available Balance"/>
       <CardAccount  titleAccount="Argent Bank Savings (x6712)" dollars="$10,928.42" accountDescription ="Available Balance"/>
       <CardAccount  titleAccount="Argent Bank Credit Card (x8349)" dollars="$184.30" accountDescription ="Current Balance"/>
-       
+       <ProfilUser/>
     </main>
 
 }
