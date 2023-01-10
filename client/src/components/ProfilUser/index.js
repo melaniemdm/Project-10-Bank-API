05b9f3c1e41f7205bb84fad3a03d2e851a639d4b
melaniemdm <melanieborja.pro@gmail.com>
2023-01-10 14:21:39 +0100

    feture(js) enregistrement new name modal userProfile

--- client/src/components/ProfilUser/index.js
+++ client/src/components/ProfilUser/index.js
@@ -5,18 +5,39 @@ import { closeModal } from '../../Utils/modal';
 import './style.scss';
 
 export default function ProfilUser(){
-  const [firstName, setFirstName] = useState('toto')   
-  const [lastName, setLastName] = useState('titi')  
+  const [firstName, setFirstName] = useState('tony')   
+  const [lastName, setLastName] = useState('stark')  
+
+
   useEffect(()=> {
     
     async function getProfile(){
       const response = await Axios.post("http://localhost:3001/api/v1/user/profile", {}, {headers: { Authorization: `Bearer `+ sessionStorage.getItem("token") }} )
       setFirstName(response.data.body.firstName)
       setLastName(response.data.body.lastName)
-    }  
-    getProfile()
+
+         }  
+       getProfile()
+
+           },[])
+
+           async function sendProfile(){
+             await Axios.put('http://localhost:3001/api/v1/user/profile',{
+                firstName: firstName,
+                lastName: lastName,
+                    
+                 }, {headers: { Authorization: `Bearer `+ sessionStorage.getItem("token") }}
+                 
+                 ) 
+         }
+         
     
-    },[])
+function saveAndValid(){
+    closeModal();
+    sendProfile()
+}
+
+
     return <div className="sign-in-content containerUser" ><div className="input-wrapper">
         <h1> Settings User</h1>
 <div>LastName : </div>
@@ -27,6 +48,6 @@ export default function ProfilUser(){
 
 <div id="mail">Mail : toto@gmail.com</div>
 
-<button className="sign-in-button" onClick={()=>closeModal()}>Valider</button>
+<button className="sign-in-button" onClick={()=>saveAndValid()}>Valider</button>
     </div></div>
 }
